9afc28192dc6952d9d3c6418acba0da2e4a4b228
jockeymacwan-dc <jockyjosaphbhai.macwan@dcmail.ca>
2024-08-07 12:53:30 -0400

    Update settings.py

--- face_attendance/settings.py
+++ face_attendance/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'py3140443@gmail.com'
-EMAIL_HOST_PASSWORD = 'ojkk tios zdgx qwme'
+EMAIL_HOST_USER = 'jockymacwan18022001@gmail.com'
+EMAIL_HOST_PASSWORD = 'hqjt xfuc texa yifw'
